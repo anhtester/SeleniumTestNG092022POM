dd7c54d701b7a8465cdc1d498a52883ea4ee5295
Anh Tester <thaian.it15@gmail.com>
2023-02-17 22:04:29 +0700

    Update lesson 25 parallel execution in TestNG

--- src/test/java/anhtester/com/common/BaseTest.java
+++ src/test/java/anhtester/com/common/BaseTest.java
@@ -5,72 +5,76 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 public class BaseTest {
 
-    public static WebDriver driver;
+      public static WebDriver driver;
 
-    @BeforeMethod
-    @Parameters({"browser"})
-    public static void createDriver(@Optional("chrome") String browser) {
-        setupDriver(browser);
-    }
+      @BeforeMethod
+      @Parameters({"browser"})
+      public static void createDriver(@Optional("chrome") String browser) {
+            setupDriver(browser);
+            
+      }
 
-    public static WebDriver setupDriver(String browserName) {
-        switch (browserName.trim().toLowerCase()) {
-            case "chrome":
-                driver = initChromeDriver();
-                break;
-            case "firefox":
-                driver = initFirefoxDriver();
-                break;
-            case "edge":
-                driver = initEdgeDriver();
-                break;
-            default:
-                System.out.println("Browser: " + browserName + " is invalid, Launching Chrome as browser of choice...");
-                driver = initChromeDriver();
-        }
-        return driver;
-    }
+      public static WebDriver setupDriver(String browserName) {
+            switch (browserName.trim().toLowerCase()) {
+                  case "chrome":
+                        driver = initChromeDriver();
+                        break;
+                  case "firefox":
+                        driver = initFirefoxDriver();
+                        break;
+                  case "edge":
+                        driver = initEdgeDriver();
+                        break;
+                  default:
+                        System.out.println("Browser: " + browserName + " is invalid, Launching Chrome as browser of choice...");
+                        driver = initChromeDriver();
+            }
+            return driver;
+      }
 
-    private static WebDriver initChromeDriver() {
-        System.out.println("Launching Chrome browser...");
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        return driver;
-    }
+      private static WebDriver initChromeDriver() {
+            System.out.println("Launching Chrome browser...");
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            return driver;
+      }
 
-    private static WebDriver initEdgeDriver() {
-        System.out.println("Launching Edge browser...");
-        WebDriverManager.edgedriver().setup();
-        driver = new EdgeDriver();
-        driver.manage().window().maximize();
-        return driver;
-    }
+      private static WebDriver initEdgeDriver() {
+            System.out.println("Launching Edge browser...");
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+            driver.manage().window().maximize();
+            return driver;
+      }
 
-    private static WebDriver initFirefoxDriver() {
-        System.out.println("Launching Firefox browser...");
-        WebDriverManager.firefoxdriver().setup();
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        return driver;
-    }
+      private static WebDriver initFirefoxDriver() {
+            System.out.println("Launching Firefox browser...");
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+            return driver;
+      }
 
-    @AfterMethod
-    public static void closeDriver() {
-        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0)); //Reset timeout
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+      @AfterMethod
+      public static void closeDriver() {
+            //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0)); //Reset timeout
+            try {
+                  Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                  throw new RuntimeException(e);
+            }
 
-        if(driver != null) {
-            driver.quit();
-        }
-    }
+            if (driver != null) {
+                  driver.quit();
+            }
+      }
 
 }
--- src/test/java/anhtester/com/testcases/LoginTest.java
+++ src/test/java/anhtester/com/testcases/LoginTest.java
@@ -6,22 +6,22 @@ import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-    LoginPage loginPage;
+      LoginPage loginPage;
 
-    @Test
-    public void loginTestSuccess(){
-        //Khởi tạo đối tượng trang LoginPage
-        //Truyền driver từ BaseTest
-        loginPage = new LoginPage(driver);
+      @Test
+      public void loginTestSuccess() {
+            //Khởi tạo đối tượng trang LoginPage
+            //Truyền driver từ BaseTest
+            loginPage = new LoginPage(driver);
 
-        //Gọi hàm "login" từ LoginPage để dùng
-        loginPage.login("admin@example.com", "123456");
-    }
+            //Gọi hàm "login" từ LoginPage để dùng
+            loginPage.login("admin@example.com", "123456");
+      }
 
-    @Test
-    public void loginTestInvalidEmail(){
-        loginPage = new LoginPage(driver);
-        loginPage.loginInvalidEmail("admin@example.com123", "123456");
-    }
+      @Test
+      public void loginTestInvalidEmail() {
+            loginPage = new LoginPage(driver);
+            loginPage.loginInvalidEmail("admin@example.com123", "123456");
+      }
 
 }
